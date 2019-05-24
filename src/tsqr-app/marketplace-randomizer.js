f65af7bd3bdec5f37229c01f98f3bb1065b51bfc
Paul Gestwicki <pvgestwicki@bsu.edu>
2019-05-24 18:58:33 -0400

    Finish marketplace

--- src/tsqr-app/marketplace-randomizer.js
+++ src/tsqr-app/marketplace-randomizer.js
@@ -16,7 +16,18 @@ class MarketplaceRandomizer extends LitElement {
             _items: { 
                 type: Array,
                 hasChanged(newVal, oldVal) {
-                    console.log('newval is ', newVal);
+                    return true;
+                }
+            },
+            _spells: { 
+                type: Array,
+                hasChanged(newVal, oldVal) {
+                    return true;
+                }
+            },
+            _weapons: { 
+                type: Array,
+                hasChanged(newVal, oldVal) {
                     return true;
                 }
             }
@@ -27,23 +38,56 @@ class MarketplaceRandomizer extends LitElement {
         super();
         this.cards = [];
         this._items = [];
+        this._weapons = [];
+        this._spells = [];
     }
 
     render() {
         return html`
           <style>
           </style>
-          <paper-button raised @click="${this._onClickFourItem}">Four-Item Marketplace</paper-button>
+          <paper-button raised @click="${this._onClick}" random>Random Marketplace</paper-button>
+          <paper-button raised @click="${this._onClick}" items=4 spells=3 weapons=3>Four-Item Marketplace</paper-button>
+          <paper-button raised @click="${this._onClick}" items=3 spells=4 weapons=3>Four-Spell Marketplace</paper-button>
+          <paper-button raised @click="${this._onClick}" items=3 spells=3 weapons=4>Four-Weapon Marketplace</paper-button>
+          Items:
           <ul>
             ${this._items.map(item => html`<li>${item.Name}</li>`)}
+          </ul>
+          Spells:
+          <ul>
+            ${this._spells.map(item => html`<li>${item.Name}</li>`)}
+          </ul>
+          Weapons:
+          <ul>
+            ${this._weapons.map(item => html`<li>${item.Name}</li>`)}
           </ul>
         `;
     }
 
-    _onClickFourItem() {
-        var oldVal = this._items;
-        this._items = selectRandomlyFrom(this.cards, 4, true);
-        this.requestUpdate('_items', oldVal);
+    _onClick(e) {
+        var items = 3;
+        var spells = 3;
+        var weapons = 3;
+        if (e.target.hasAttribute("random")) {
+            switch (Math.floor(Math.random()*3)) {
+                case 0: items = 4; break;
+                case 1: spells = 4; break;
+                case 2: weapons = 4; break;
+                default: throw "Unreachable case";
+            }
+        }
+        else {
+            items = Number(e.target.getAttribute("items"));
+            spells = Number(e.target.getAttribute("spells"));
+            weapons = Number(e.target.getAttribute("weapons"));
+        }
+        this._items = selectRandomlyFrom(this.cards, items, true);
+        this._spells = selectRandomlyFrom(this.cards, spells, true);
+        this._weapons = selectRandomlyFrom(this.cards, weapons, true);
+        this.requestUpdate('_items');
+        this.requestUpdate('_spells');
+        this.requestUpdate('_weapons');
     }
 
 }
